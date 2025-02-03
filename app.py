9728cd49efedfc95037e9854fd3a48a0f9c3200a
Shivam Dubey <96882359+Dubeyrock@users.noreply.github.com>
2025-02-03 18:13:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,9 +72,9 @@ def main():
     st.set_page_config(page_title="AI Assistive Tool", layout="wide", page_icon="🤖")
 
     # Adding background image to sidebar
-    sidebar_image_path = r'C:\Users\dubey\Desktop\New folder (10)\AI-Assistive-Tool-for-Visually-Impaired-\back_ground.jpg'
-    sidebar_image = Image.open(sidebar_image_path)
-    st.sidebar.image(sidebar_image, use_column_width=True)
+   # sidebar_image_path = r'C:\Users\dubey\Desktop\New folder (10)\AI-Assistive-Tool-for-Visually-Impaired-\back_ground.jpg'
+    #sidebar_image = Image.open(sidebar_image_path)
+   # st.sidebar.image(sidebar_image, use_column_width=True)
 
     st.title('AI Assistive Tool for Visually Impaired 👁️ 🤖')
     # Project Overview
